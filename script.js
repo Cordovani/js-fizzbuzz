35dbf42c4e41edb38a4dfc7d7ff73e64693ffe7d
Cordovani <lor.cordovani@gmail.com>
2021-04-12 14:56:16 +0200

    inizializzazione e struttura dell'esercizio

---
+++ script.js
@@ -0,0 +1,9 @@
+// stampare i 100 numeri 
+
+// verificare le condizioni
+// se è divisibile per 3 stampo Fizz
+// se è divisibile per 5 stampo Buzz
+// se è divisibile per 5 e per 3 stampo FizzBuzz
+
+// stampo l'output
+
